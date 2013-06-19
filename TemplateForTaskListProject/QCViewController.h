ceb9c505a54f9f6afa3780315efdaaf152a6b458
Eliot Arntz <arntzel@gmail.com>
2013-06-19 13:24:04 -0400

    Initial Commit

---
+++ TemplateForTaskListProject/QCViewController.h
@@ -0,0 +1,13 @@
+//
+//  QCViewController.h
+//  TemplateForTaskListProject
+//
+//  Created by Eliot Arntz on 6/19/13.
+//  Copyright (c) 2013 self.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QCViewController : UIViewController
+
+@end
